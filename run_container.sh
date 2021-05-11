d72931373823464964f0e2d10d8ae1c0415ffb41
Francis Eudy <francis.eudy@gmail.com>
2021-05-11 09:41:56 -0400

    change port

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run --rm -p 8000:8888 --user root -e NB_USER=ryanfeudy -e JUPYTER_ENABLE_LAB=yes -e CHOWN_HOME=yes -v "$PWD":/home/ryanfeudy/work jupyter-server-hands-on-ml
+docker run --rm -p 8070:8888 --user root -e NB_USER=ryanfeudy -e JUPYTER_ENABLE_LAB=yes -e CHOWN_HOME=yes -v "$PWD":/home/ryanfeudy/work jupyter-server-hands-on-ml
